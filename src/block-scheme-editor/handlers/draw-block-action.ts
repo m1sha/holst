b720e703a1a8b2bb69309ae8d5351f518a74602e
m1sha <m1sha@live.ru>
2021-10-13 13:00:12 +0700

    text on blocks

--- src/block-scheme-editor/handlers/draw-block-action.ts
+++ src/block-scheme-editor/handlers/draw-block-action.ts
@@ -3,11 +3,17 @@ import { rect } from '../../core/utils'
 import { Block } from '../elements/block'
 
 export default function (s: Scene, block: Block) {
-  let color = block.selected ? '#411673' : '#F11333'
+  let color = block.selected ? '#411673' : '#991333'
   if (block.hovered) {
     color = '#F18891a'
   }
-  const shape = s.createLayer().createShape({ fillStyle: color })
+  const layer = s.createLayer()
+  const shape = layer.createShape({ fillStyle: color })
   shape.name = block._uid.toString()
-  shape.rect(rect(block.position.x, block.position.y, 50, 50))
+
+  const textStyle = { fontSize: '16px', color: '#fff' }
+  const width = layer.measureText(block.text, textStyle).width
+
+  shape.rect(rect(block.position.x, block.position.y, width + 10, 50))
+  layer.createText({ value: block.text, x: w => block.position.x + 5, y: w => block.position.y + 25, style: textStyle })
 }
