fe85557d12218f9bee2f625fc4d212ac442ddc47
m1sha <m1sha@live.ru>
2022-02-19 17:12:02 +0700

    Constraint Grid

---
+++ src/core/constraint-grid.ts
@@ -0,0 +1,41 @@
+import { Size } from './size'
+import { Rect } from './rect'
+import { Point } from './point'
+import { point, rect } from './utils'
+
+export type ConstraintGridCell = { rect: Rect, center: Point }
+export class ConstraintGrid {
+  readonly size: Readonly<Size>
+  readonly rows: number
+  readonly columns: number
+  cells: ConstraintGridCell[]
+  constructor (size: Size, rows: number, columns: number) {
+    this.size = size
+    this.rows = rows
+    this.columns = columns
+    this.cells = []
+    this.calculate()
+  }
+
+  getCell (row: number, columns: number) {
+    const index = (row * this.columns) + columns
+    return this.cells[index]
+  }
+
+  private calculate () {
+    const { width, height } = this.size
+    const { columns, rows } = this
+    const cellWidth = width / columns
+    const cellHeight = height / rows
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        const x = cellWidth * j
+        const y = cellHeight * i
+        this.cells.push({
+          rect: rect(x, y, cellWidth, cellHeight),
+          center: point(cellWidth / 2 + x, cellHeight / 2 + y)
+        })
+      }
+    }
+  }
+}
---
+++ tests/constraint-grid.test.ts
@@ -0,0 +1,15 @@
+import { ConstraintGrid } from '../src/core/constraint-grid'
+
+test('Constraint Grid 3x3', () => {
+  const grid = new ConstraintGrid({ width: 300, height: 300 }, 3, 3)
+  expect(grid.getCell(0, 0).center).toEqual({ x: 50, y: 50 })
+  expect(grid.getCell(1, 1).center).toEqual({ x: 150, y: 150 })
+  expect(grid.getCell(1, 2).center).toEqual({ x: 250, y: 150 })
+  expect(grid.getCell(2, 2).center).toEqual({ x: 250, y: 250 })
+  expect(grid.getCell(2, 0).center).toEqual({ x: 50, y: 250 })
+})
+
+test('Constraint Grid 9x9', () => {
+  const grid = new ConstraintGrid({ width: 300, height: 300 }, 9, 9)
+  expect(grid.getCell(4, 4).center).toEqual({ x: 150, y: 150 })
+})
