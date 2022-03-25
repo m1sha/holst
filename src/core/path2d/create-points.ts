15dffa93157b3a298b7026cdf102fe8f30c64796
m1sha <m1sha@live.ru>
2022-03-25 21:06:29 +0700

    createPoints refact.

--- src/core/path2d/create-points.ts
+++ src/core/path2d/create-points.ts
@@ -1,75 +1,79 @@
 import { Matrix2D } from '../matrix'
 import { Point } from '../point'
 import { Path2DElement } from './types/path2d-element'
+type HandlerDelegate = (arr: Point[], element: Path2DElement, transform: Matrix2D, stack: Path2DElement[], index: number) => void
+const handlers: Record<string, HandlerDelegate> = {}
+
+handlers.Arc = (arr, element, transform) => {
+  if (element.type !== 'Arc') return
+  const p = transform.applyMatrix(element)
+  arr.push(p)
+  arr.push(new Point(p.x, p.y + element.radius))
+  arr.push(new Point(p.x, p.y + -element.radius))
+  arr.push(new Point(p.y, p.y + element.radius))
+  arr.push(new Point(p.x, p.y + -element.radius))
+}
+
+handlers.ArcTo = (arr, element, transform) => {
+  if (element.type !== 'ArcTo') return
+  const p1 = transform.applyMatrix(new Point(element.x1, element.y1))
+  const p2 = transform.applyMatrix(new Point(element.x2, element.y2))
+  arr.push(p1)
+  arr.push(p2)
+  arr.push(new Point(p1.x, p1.y + element.radius))
+  arr.push(new Point(p1.x, p1.y + -element.radius))
+  arr.push(new Point(p1.y, p1.y + element.radius))
+  arr.push(new Point(p1.x, p1.y + -element.radius))
+}
+
+handlers.BezierCurveTo = (arr, element, transform) => {
+  if (element.type !== 'BezierCurveTo') return
+  const p = transform.applyMatrix(element)
+  arr.push(p)
+}
+
+handlers.ClosePath = () => {}
+
+handlers.Ellipse = (arr, element, transform) => {
+  if (element.type !== 'Ellipse') return
+  const p = transform.applyMatrix(element)
+  arr.push(p)
+  arr.push(new Point(p.x, element.radiusX))
+  arr.push(new Point(p.x, -element.radiusX))
+  arr.push(new Point(p.y, element.radiusY))
+  arr.push(new Point(p.x, -element.radiusY))
+}
+
+handlers.LineTo = (arr, element, transform) => {
+  if (element.type !== 'LineTo') return
+  const p = transform.applyMatrix(element)
+  arr.push(p)
+}
+
+handlers.MoveTo = (arr, element, transform) => {
+  if (element.type !== 'MoveTo') return
+  const p = transform.applyMatrix(element)
+  arr.push(p)
+}
+
+handlers.QuadraticCurveTo = (arr, element, transform) => {
+  if (element.type !== 'QuadraticCurveTo') return
+  const p = transform.applyMatrix(element)
+  arr.push(p)
+  arr.push(new Point(p.x, element.cpx))
+  arr.push(new Point(p.y, element.cpy))
+}
+
+handlers.Rect = (arr, element, transform) => {
+  if (element.type !== 'Rect') return
+  const p = transform.applyMatrix(element)
+  arr.push(p)
+  arr.push(new Point(p.x + element.w, p.y + element.h))
+}
 
 export function createPoints (stack: Path2DElement[], transform: Matrix2D): Point[] {
   const result: Point[] = []
-  for (const i of stack) {
-    switch (i.type) {
-      case 'Arc': {
-        const p = transform.applyMatrix(i)
-        result.push(p)
-        result.push(new Point(p.x, i.radius))
-        result.push(new Point(p.x, -i.radius))
-        result.push(new Point(p.y, i.radius))
-        result.push(new Point(p.x, -i.radius))
-        continue
-      }
-      case 'ArcTo': {
-        const p1 = transform.applyMatrix(new Point(i.x1, i.y1))
-        const p2 = transform.applyMatrix(new Point(i.x2, i.y2))
-        result.push(p1)
-        result.push(p2)
-        result.push(new Point(p1.x, i.radius))
-        result.push(new Point(p1.x, -i.radius))
-        result.push(new Point(p1.y, i.radius))
-        result.push(new Point(p1.x, -i.radius))
-        continue
-      }
-      case 'BezierCurveTo': {
-        const p = transform.applyMatrix(i)
-        result.push(p)
-        // result.push(p.x + i.cp1x)
-        // result.push(p.x + i.cp2x)
-        // result.push(p.y + i.cp1y)
-        // result.push(p.y + i.cp2y)
-        continue
-      }
-      case 'ClosePath':
-        continue
-      case 'Ellipse': {
-        const p = transform.applyMatrix(i)
-        result.push(p)
-        result.push(new Point(p.x, i.radiusX))
-        result.push(new Point(p.x, -i.radiusX))
-        result.push(new Point(p.y, i.radiusY))
-        result.push(new Point(p.x, -i.radiusY))
-        continue
-      }
-      case 'LineTo': {
-        const p = transform.applyMatrix(i)
-        result.push(p)
-        continue
-      }
-      case 'MoveTo': {
-        const p = transform.applyMatrix(i)
-        result.push(p)
-        continue
-      }
-      case 'QuadraticCurveTo': {
-        const p = transform.applyMatrix(i)
-        result.push(p)
-        result.push(new Point(p.x, i.cpx))
-        result.push(new Point(p.y, i.cpy))
-        continue
-      }
-      case 'Rect': {
-        const p = transform.applyMatrix(i)
-        result.push(p)
-        result.push(new Point(p.x + i.w, p.y + i.h))
-        continue
-      }
-    }
-  }
+  let i = 0
+  for (const element of stack) handlers[element.type](result, element, transform, stack, i++)
   return result
 }
