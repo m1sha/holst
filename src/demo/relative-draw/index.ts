d00742821454e1be30b12462ac134d964b7fd779
m1sha <m1sha@live.ru>
2022-04-18 22:55:10 +0700

    fix relative demo

--- src/demo/relative-draw/index.ts
+++ src/demo/relative-draw/index.ts
@@ -1,13 +1,14 @@
-import { Point, Rect, Renderer2D, Scene } from '@/.'
+import { Point, Rect, Renderer2D, Scene } from 'index'
 
 export function createRelativeDrawExample (canvas: HTMLCanvasElement) {
   const scene = new Scene()
+  const bg = scene.createLayer()
   const layer = scene.createLayer()
 
-  layer.createShape({ fillStyle: '#eee' })
+  bg.createShape({ fillStyle: '#1ee' })
     .rect(new Rect(0, 0, 100, 100))
 
-  const pos = layer.createShape({ strokeStyle: 'blue', lineWidth: 2 })
+  const pos = layer.createShape({ strokeStyle: 'blue', lineWidth: 4, lineCap: 'round' })
     .relative
     .moveTo(new Point(20, 20))
     .lineTo(new Point(0, 20))
@@ -18,7 +19,12 @@ export function createRelativeDrawExample (canvas: HTMLCanvasElement) {
   layer.createShape({ strokeStyle: 'green', lineWidth: 1 })
     .relative
     .moveTo(pos)
-    .ellipse(10, 10, Math.PI, 0, Math.PI * 2)
+    .ellipse(10, 10, 0, 0, Math.PI * 2)
+
+  const b = layer.bounds
+
+  layer.createShape({ strokeStyle: 'red' })
+    .rect(b)
 
   const renderer = new Renderer2D(canvas.getContext('2d')!!)
   renderer.render(scene)
