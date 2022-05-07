451efcba7e8651499b96fe7a28aa2fafe04a42e3
m1sha <m1sha@live.ru>
2022-05-07 17:54:03 +0700

    arrange

--- src/core/arrange.ts
+++ src/core/arrange.ts
@@ -1,18 +1,30 @@
 import Orderable from './orderable'
+import { sort } from './sorter'
 
 export class Arrange {
-  private counter: number = 0
   readonly orderableList: Orderable[]
   constructor (orderableList: Orderable[]) {
     this.orderableList = orderableList
   }
 
-  sendToBack (orderable: Orderable) {}
+  sendToBack (orderable: Orderable) {
+    const sorted = this.sorted
+    const length = sorted.length
+    sorted.indexOf(orderable)
+    for (let i = 0; i < length; i++) {
+      //
+    }
+  }
+
   sendToBackward (orderable: Orderable) {}
   bringToFront (orderable: Orderable) {}
   bringToForward (orderable: Orderable) {}
 
   get order () {
-    return ++this.counter
+    return Math.max.apply(null, this.orderableList.map(p => p.order)) + 1
+  }
+
+  private get sorted () {
+    return sort(this.orderableList)
   }
 }
