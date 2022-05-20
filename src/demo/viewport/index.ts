d1019be38d005b91b3c92b4367f701ae3aab96df
m1sha <m1sha@live.ru>
2022-05-21 00:44:50 +0700

    shape move

--- src/demo/viewport/index.ts
+++ src/demo/viewport/index.ts
@@ -1,4 +1,4 @@
-import { Scene, Renderer2D, Rect } from 'index'
+import { Scene, Renderer2D, Rect, Point } from 'index'
 import { ScrollBox } from '../../core/components/scroll-box'
 export function createViewportDemo (canvas: HTMLCanvasElement) {
   const scene = createScene()
@@ -17,14 +17,38 @@ function createScene (): Scene {
   const scene = new Scene()
   const layer0 = scene.createLayer()
 
-  layer0
+  const s0 = layer0
     .createShape({ strokeStyle: '#333' })
     .rect(new Rect(10, 10, 150, 150))
+
+  s0.move({ x: 100, y: 10 })
+
+  let state = 0
+  let point = new Point(0, 0)
   const s = layer0.createShape({ fillStyle: '#ff00ff' })
-  s.rect(new Rect(600, 10, 150, 150))
-    .on('click', () => alert('Rect'))
+    .rect(new Rect(600, 10, 150, 150))
+  s
+    // .on('click', () => alert('Rect'))
     .on('hover', e => (s.style.fillStyle = '#1ff01f'))
     .on('leave', e => (s.style.fillStyle = '#ff00ff'))
+    .on('mousedown', e => {
+      state = 1
+      point = new Point(e.event.offsetX, e.event.offsetY)
+    })
+    .on('mouseup', () => {
+      state = 0
+      point = new Point(0, 0)
+    })
+    .on('mousemove', e => {
+      if (state === 1) {
+        if (point.x === 0 && point.y === 0) return
+        // console.log('x: %d y: %d', e.event.offsetX, e.event.offsetY)
+        let point2 = new Point(e.event.offsetX, e.event.offsetY)
+        point2 = point2.dec(point)
+        s.move(point2)
+      }
+    })
+
   layer0.createShape({ fillStyle: '#510051', strokeStyle: '#3f3f53', lineWidth: 12 })
     .arc({ x: 500, y: 310 }, 50, Math.PI, Math.PI * 2)
   layer0.createShape({ fillStyle: '#113051', strokeStyle: '#3f3ff3', lineWidth: 4 })
