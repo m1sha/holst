28329243dac50d53e3e0f560de56753c4f21c3be
m1sha <m1sha@live.ru>
2022-06-04 22:36:48 +0700

    collisionsDetect

--- src/demo/collisions/index.ts
+++ src/demo/collisions/index.ts
@@ -1,8 +1,6 @@
-import { Deformation } from '../../core/modifiers/deformation'
 import { Scene, Renderer2D, Point, Shape } from 'index'
 import { Size } from '../../core/size'
 import { getColor } from './colors'
-import { Figure } from '../../core/primitives/types/figures'
 
 interface Element {
   shape: Shape
@@ -24,32 +22,56 @@ export function createCollisionsDemo (canvas: HTMLCanvasElement) {
 function initScene (scene: Scene, { width, height }: Size): Element[] {
   const result = []
   const layer = scene.createLayer('collisions')
-  const elementCount = 1
+  const elementCount = 3
   for (let i = 0; i < elementCount; i++) {
+    // const frame = layer.createShape({ strokeStyle: getColor() })
     const shape = layer.createShape({ strokeStyle: getColor(), fillStyle: getColor() })
-    const x = Math.floor(Math.random() * width - 30)
-    const y = Math.floor(Math.random() * height - 30)
-    const r = Math.floor(Math.random() * 16) + 10
+    const x = Math.floor(Math.random() * width - 150) + 100
+    const y = Math.floor(Math.random() * height - 150) + 100
+    const r = Math.floor(Math.random() * 30) + 25
     shape.circle({ x, y }, r)
     const el: Element = {
       shape,
       shift: new Point(0, 0),
-      velocity: new Point(1, 0)
+      velocity: new Point(Math.random() * 7 + 2, (Math.random() * 7 + 2))
     }
     // shape.addModifier(new Deformation(f => onShapeDeformation(el, f)))
-    shape.circles[0].r = 100
+    // shape.circles[0].r = 35
     result.push(el)
+    // frame.rect(shape.bounds.outline(-8))
   }
   return result
 }
 
-// function onShapeDeformation (el: Element, f: Figure) {
-//   // f.circle[0].r = 2
-//   el.shape.move(el.shift)
-// }
-
 function update (elements: Element[], size: Size) {
+  collisionsDetect(elements)
   for (const el of elements) {
-    el.shift = el.shift.add(el.velocity)
+    const c = el.shape.circles[0]
+    if (c.x + c.r > size.width || c.x - c.r < 0) el.velocity.x *= -1
+    if (c.y + c.r > size.height || c.y - c.r < 0) el.velocity.y *= -1
+    el.shape.circles[0].x += el.velocity.x
+    el.shape.circles[0].y += el.velocity.y
+  }
+}
+
+function collisionsDetect (elements: Element[]) {
+  let i = 0
+  let j = 0
+  for (const el of elements) {
+    const c = el.shape.circles[0]
+    for (const el2 of elements) {
+      if (i === j) continue
+      const c2 = el2.shape.circles[0]
+      if (c.x > c2.x - c2.r && c.x < c2.x + c2.r) {
+        el.velocity.x *= -1
+        // el2.velocity.x *= -1
+      }
+      if (c.y > c2.y - c2.r && c.y < c2.y + c2.r) {
+        el.velocity.y *= -1
+        // el2.velocity.y *= -1
+      }
+      j++
+    }
+    i++
   }
 }
