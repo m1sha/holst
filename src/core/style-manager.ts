38796c3fcad0e340fe5e720aff4cba3e3aa21050
m1sha <m1sha@live.ru>
2022-06-07 00:29:08 +0700

    ShapeStyleDecorator

--- src/core/style-manager.ts
+++ src/core/style-manager.ts
@@ -1,5 +1,5 @@
-// /* global CanvasLineCap, CanvasLineJoin */
-// import { Color } from './color'
+/* global CanvasLineCap, CanvasLineJoin */
+import { Color } from './color'
 import { TextStyle } from './label-style'
 import { ShapeStyle } from './shape-style'
 
@@ -15,7 +15,7 @@ export class StyleManager {
   }
 
   shapes (name: string) {
-    return this.styles[name] as ShapeStyle
+    return new ShapeStyleDecorator(this.styles[name] as ShapeStyle)
   }
 
   texts (name: string) {
@@ -23,16 +23,34 @@ export class StyleManager {
   }
 }
 
-// class ShapeStyleImpl implements ShapeStyle {
-//   lineCap?: CanvasLineCap
-//   lineDashOffset?: number
-//   lineDash?: number[]
-//   lineJoin?: CanvasLineJoin
-//   lineWidth?: number
-//   miterLimit?: number
-//   fillStyle?: string | CanvasGradient | CanvasPattern | Color
-//   strokeStyle?: string | CanvasGradient | CanvasPattern | Color
-//   constructor (style: ShapeStyle) {
-//     this.strokeStyle = style.strokeStyle
-//   }
-// }
+class ShapeStyleDecorator implements ShapeStyle {
+  lineCap?: CanvasLineCap
+  lineDashOffset?: number
+  lineDash?: number[]
+  lineJoin?: CanvasLineJoin
+  lineWidth?: number
+  miterLimit?: number
+  fillStyle?: string | CanvasGradient | CanvasPattern | Color
+  strokeStyle?: string | CanvasGradient | CanvasPattern | Color
+  constructor (style: ShapeStyle) {
+    this.assign(style)
+  }
+
+  assign (style?: ShapeStyle) {
+    if (style && !isUndefined(style.strokeStyle)) this.strokeStyle = style.strokeStyle
+    if (style && !isUndefined(style.lineDashOffset)) this.lineDashOffset = style.lineDashOffset
+    if (style && !isUndefined(style.lineDash)) this.lineDash = style.lineDash
+    if (style && !isUndefined(style.lineJoin)) this.lineJoin = style.lineJoin
+    if (style && !isUndefined(style.lineWidth)) this.lineWidth = style.lineWidth
+    if (style && !isUndefined(style.miterLimit)) this.miterLimit = style.miterLimit
+    if (style && !isUndefined(style.fillStyle)) this.fillStyle = style.fillStyle
+    if (style && !isUndefined(style.strokeStyle)) this.strokeStyle = style.strokeStyle
+    return this
+  }
+
+  clone (style?: ShapeStyle): ShapeStyleDecorator {
+    return new ShapeStyleDecorator(this).assign(style)
+  }
+}
+
+const isUndefined = (t: unknown) => typeof t === 'undefined' || t === undefined
