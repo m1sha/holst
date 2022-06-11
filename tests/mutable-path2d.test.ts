382b2bc7eb15197375c59fe782e0d54b59a10133
m1sha <m1sha@live.ru>
2022-06-11 22:58:29 +0700

    recover test

--- tests/mutable-path2d.test.ts
+++ tests/mutable-path2d.test.ts
@@ -6,5 +6,5 @@ test('MutablePath2D ', () => {
   const stack: Path2DElement[] = []
   const path = new MutablePath2D(stack)
   path.rect(10, 10, 100, 100)
-  expect(stack[0]).toEqual({ type: 'Rect', x: 10, y: 10, w: 100, h: 100 })
+  expect(stack[0]).toEqual({ type: 'Rect', x: 10, y: 10, width: 100, height: 100 })
 })
