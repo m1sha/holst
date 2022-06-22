4846b6dd615447a62c90b7414d825dfabc6ece08
m1sha <m1sha@live.ru>
2022-06-22 11:16:21 +0700

    FontWeight for TextStyle

--- src/core/label-style.ts
+++ src/core/label-style.ts
@@ -1,9 +1,11 @@
 import { Color } from './color'
 
+export type FontWeight = 'normal' | 'bold' | 'lighter' | 'bolder' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900'
+
 export interface TextStyle {
   color?: string | Color
   fontName?: string
   fontSize?: string
-  bold? : boolean | '400' | '500' | '600' | '700'
+  bold? : boolean | FontWeight
   italic?: boolean
 }
