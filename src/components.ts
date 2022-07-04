6d5ddb4297d4896811967c7f1963d66868c2e026
m1sha <m1sha@live.ru>
2022-07-04 22:55:21 +0700

    fix exports

--- src/components.ts
+++ src/components.ts
@@ -1,13 +1,12 @@
 import { Table } from './core/components/table/table'
 import { Row } from './core/components/table/row'
 import { Cell } from './core/components/table/cell'
-import { TableControl } from './core/components/table/table-control'
+export * from './core/components/table/table-control'
 export * from './core/components/component'
 export * from './core/components/table/table-designer'
 
 export {
   Table,
   Row,
-  Cell,
-  TableControl
+  Cell
 }
