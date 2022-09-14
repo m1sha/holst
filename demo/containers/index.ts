baf07ed3369f187844ef5f126e028720522ffe76
m1sha <m1sha@live.ru>
2022-09-14 22:46:53 +0700

    Containers Demo

--- demo/containers/index.ts
+++ demo/containers/index.ts
@@ -10,22 +10,26 @@ export async function createContainersDemo (canvas: HTMLCanvasElement) {
 
   const layer = scene.createLayer()
 
-  layer.createShape({ fill: Color.red }).circle(300, 100, 50)
+  const circle = layer.createShape({ fill: Color.red }).circle(300, 100, 50)
 
   const shape = layer.createShape({ fill: '#115511' }).rect(10, 10, 100, 100)
   const anchor = Anchor.create(shape)
-  layer.createShape({ fill: '#fff' }).rect(10, 10, 20, 20).setAnchor(anchor)
+  const whiteRect = layer.createShape({ fill: '#fff' }).rect(10, 10, 20, 20)
+  // whiteRect.order = 2
+  whiteRect.setAnchor(anchor)
+  whiteRect.hidden = true
 
   layer.addRaster(raster)
   raster.setAnchor(anchor)
   raster.distRect = new Rect(10, 40, 150, 150)
-  raster.order = 5
+  // raster.order = 5
 
   let delta = Point.zero
   shape
     .on('hover', () => shape.shadow.set({ x: 10, y: 4 }, 8, '#aaa'))
     .on('leave', () => shape.shadow.clear())
     .on('mousedown', e => {
+      e.event.stopPropagation()
       const p = { x: e.event.origin.offsetX, y: e.event.origin.offsetY }
       delta = new Point(shape.figures.first() as IPoint).dec(p)
     })
@@ -36,6 +40,7 @@ export async function createContainersDemo (canvas: HTMLCanvasElement) {
       if (!e.event.pressed) {
         return
       }
+      e.event.stopPropagation()
 
       e.cursor = 'move'
       const p = { x: e.event.origin.offsetX, y: e.event.origin.offsetY }
@@ -43,6 +48,42 @@ export async function createContainersDemo (canvas: HTMLCanvasElement) {
       rect.x = delta.x + p.x
       rect.y = delta.y + p.y
     })
+    .on('dblclick', e => {
+      console.log('main shape shape order is ' + shape.order)
+    })
+
+  let delta2 = Point.zero
+  circle
+    .on('hover', () => circle.shadow.set({ x: 10, y: 4 }, 8, '#aaa'))
+    .on('leave', () => circle.shadow.clear())
+    .on('mousedown', e => {
+      e.event.stopPropagation()
+      const p = { x: e.event.origin.offsetX, y: e.event.origin.offsetY }
+      delta2 = new Point(circle.figures.last() as IPoint).dec(p)
+    })
+    .on('mouseup', e => {
+      e.cursor = 'default'
+    })
+    .on('mousemove', e => {
+      if (!e.event.pressed) {
+        return
+      }
+      e.event.stopPropagation()
+
+      e.cursor = 'move'
+      const p = { x: e.event.origin.offsetX, y: e.event.origin.offsetY }
+      const rect = circle.figures.last() as IPoint
+      rect.x = delta2.x + p.x
+      rect.y = delta2.y + p.y
+    })
+    .on('dblclick', e => {
+      console.log('main shape shape order is ' + shape.order)
+    })
+
+  whiteRect.on('dblclick', e => {
+    e.event.stopPropagation()
+    console.log('whiteRect order is ' + whiteRect.order)
+  })
 
   const renderer = new Renderer2D(canvas.getContext('2d')!!)
   renderer.render(scene)
