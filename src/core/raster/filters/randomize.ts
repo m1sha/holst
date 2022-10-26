5a4e8470bf9bc2664cf0dc7306a00c5d8f006442
m1sha <m1sha@live.ru>
2022-10-26 20:39:01 +0700

    Randomize filter: monochromic mode

--- src/core/raster/filters/randomize.ts
+++ src/core/raster/filters/randomize.ts
@@ -3,9 +3,11 @@ import { truncate } from './helpers/truncate'
 
 export class Randomize implements Filter {
   private value: number
+  private monochromic: boolean
 
-  constructor (value: number) {
+  constructor (value: number, monochromic?: boolean) {
     this.value = value
+    this.monochromic = monochromic ?? false
   }
 
   apply (imageData: ImageData) {
@@ -14,9 +16,17 @@ export class Randomize implements Filter {
     const sign = () => Math.random() > 0.5 ? -1 : 1
 
     for (let i = 0; i < data.length; i += 4) {
-      data[i] = truncate(data[i] + seed() * sign())
-      data[i + 1] = truncate(data[i + 1] + seed() * sign())
-      data[i + 2] = truncate(data[i + 2] + seed() * sign())
+      if (Math.random() > 0.5) continue
+      if (this.monochromic) {
+        const v = truncate((data[i] + data[i + 1] + data[i + 2]) / 3 + seed() * sign())
+        data[i] = v
+        data[i + 1] = v
+        data[i + 2] = v
+      } else {
+        data[i] = truncate(data[i] + seed() * sign())
+        data[i + 1] = truncate(data[i + 1] + seed() * sign())
+        data[i + 2] = truncate(data[i + 2] + seed() * sign())
+      }
     }
   }
 }
