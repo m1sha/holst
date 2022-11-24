9d72764f4f3d9245a88253dd488b835153befc2f
m1sha <m1sha@live.ru>
2022-11-24 18:27:06 +0700

    fixed examples

--- demo/shape-bounds/index.ts
+++ demo/shape-bounds/index.ts
@@ -1,41 +1,58 @@
-import { Scene, Renderer2D, ShapeStyle, Shape, Color, Rect, Drawable, Layer, ConstraintGrid } from '../../src/index'
+import { Scene, Renderer2D, ShapeStyle, Shape, Color, Rect, Drawable, Layer, ConstraintGrid, Size } from '../../src/index'
 export function createDemo (div: HTMLDivElement) {
   const scene = new Scene()
   const layer = scene.createLayer()
   const grid = new ConstraintGrid(new Rect(0, 0, 800, 600), 3, 4)
+  const figureSize: Size = { width: 80, height: 80 }
 
   const rect = createFigure(layer)
-    .rect(getRect(grid, 0, 0, 50, 50))
+    .rect(getRect(grid, 0, 0, figureSize.width, figureSize.height))
   addFrame(rect, layer)
 
   const roundRect = createFigure(layer)
-    .roundRect(getRect(grid, 0, 1, 50, 50), 12)
+    .roundRect(getRect(grid, 0, 1, figureSize.width, figureSize.height), 12)
   addFrame(roundRect, layer)
 
   const circle = createFigure(layer)
-    .circle(getCenter(grid, 0, 2), 25)
+    .circle(getCenter(grid, 0, 2), figureSize.width / 2)
   addFrame(circle, layer)
 
   const segmentedCircle = createFigure(layer)
-    .segmentedCircle(getCenter(grid, 0, 3).x, getCenter(grid, 0, 3).y, 25, 6, 0.5)
+    .segmentedCircle(getCenter(grid, 0, 3).x, getCenter(grid, 0, 3).y, figureSize.width / 2, 6, 0.5)
   addFrame(segmentedCircle, layer)
 
-  const moveToLineToRect = getRect(grid, 1, 0, 50, 50)
+  const moveToLineToRect = getRect(grid, 1, 0, figureSize.width, figureSize.height)
   const moveToLineTo = createFigure(layer)
     .moveTo(moveToLineToRect.topLeft).lineTo(moveToLineToRect.bottomRight)
   addFrame(moveToLineTo, layer)
 
-  const bezierCurveToRect = getRect(grid, 1, 1, 50, 50)
+  const bezierCurveToRect = getRect(grid, 1, 1, figureSize.width, figureSize.height)
   const bezierCurveTo = createFigure(layer)
     .moveTo(bezierCurveToRect.topLeft)
     .bezierCurveTo(bezierCurveToRect.topLeft, bezierCurveToRect.bottomRight.add(-25, 25), bezierCurveToRect.bottomRight)
   addFrame(bezierCurveTo, layer)
 
-  const quadraticCurveToRect = getRect(grid, 1, 2, 50, 50)
+  const quadraticCurveToRect = getRect(grid, 1, 2, figureSize.width, figureSize.height)
   const quadraticCurveTo = createFigure(layer)
     .moveTo(quadraticCurveToRect.topLeft.add(-10))
     .quadraticCurveTo(quadraticCurveToRect.topLeft, quadraticCurveToRect.bottomRight.add(-25, 25))
   addFrame(quadraticCurveTo, layer)
+
+  const arc = createFigure(layer)
+    .arc(getCenter(grid, 1, 3), figureSize.width / 2, Math.PI / 4, Math.PI)
+  addFrame(arc, layer)
+
+  const ellipse = createFigure(layer)
+    .ellipse(getCenter(grid, 2, 0), figureSize.width / 2, 35, 0, Math.PI / 4, Math.PI)
+  addFrame(ellipse, layer)
+
+  const arcFull = createFigure(layer)
+    .arc(getCenter(grid, 2, 1), figureSize.width / 2, 0, Math.PI * 2)
+  addFrame(arcFull, layer)
+
+  const ellipseFull = createFigure(layer)
+    .ellipse(getCenter(grid, 2, 2), figureSize.width / 2, 35, 0, 0, Math.PI * 2)
+  addFrame(ellipseFull, layer)
 
   createView(scene, div)
 }
