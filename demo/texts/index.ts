cb3590c9cd784fcf95ec4524f8ea4953fe5b3ea7
m1sha <m1hailostertag@gmail.com>
2023-02-19 22:34:13 +0700

    removed demo

--- demo/texts/index.ts
+++
@@ -1,3 +0,0 @@
-import { App } from './components/app'
-
-export const createDemo = (app: HTMLDivElement) => (new App().create(app))
--- demo/texts/model/entities/entity-readonly.ts
+++
@@ -1,4 +0,0 @@
-import { Drawable } from '../../../../src'
-import { Entity } from './entity'
-
-export type EntityReadonly<T extends Drawable> = Readonly<Entity<T>>
